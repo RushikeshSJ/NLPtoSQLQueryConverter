325c2c1eee4a63de388222d1b30e42121bf59754
Rushikesh Jadhav <43134782+RushikeshSJ@users.noreply.github.com>
2025-06-08 19:53:20 +0530

    EC2 AWS URL Updated

--- NLPtoSQLQueryConverterFrontEnd/nlptosql/src/models/queryService.js
+++ NLPtoSQLQueryConverterFrontEnd/nlptosql/src/models/queryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'ec2-98-130-50-18.ap-south-2.compute.amazonaws.com/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
